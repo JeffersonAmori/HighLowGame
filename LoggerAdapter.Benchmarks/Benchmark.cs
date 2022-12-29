615a2153b79e99c5d513fb3893485b00705a1239
JeffersonRochaBT <jefferson.amoridarocha@budgetthuis.nl>
2022-12-29 01:35:17 +0100

    Added benchmark for LoggerAdapter

---
+++ LoggerAdapter.Benchmarks/Benchmark.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using LoggerAdapter;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace LoggerAdapter.Benchmarks;
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    [Params(100, 1000, 10000)]
+    public int Count;
+
+    [Benchmark]
+    public void LoggerAdapter_WithValueType()
+    {
+        var mock = new Mock<ILoggerAdapter<Benchmark>>();
+        var logger = mock.Object;
+
+        for (var i = 0; i < Count; i++)
+        {
+            logger.LogInformation("Message: {message}", 2);
+        }
+    }
+
+    [Benchmark]
+    public void LoggerAdapter_WithReferenceType()
+    {
+        var mock = new Mock<ILoggerAdapter<Benchmark>>();
+        var logger = mock.Object;
+
+        for (var i = 0; i < Count; i++)
+        {
+            logger.LogInformation("Message: {message}", "reference type");
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public void ILogger_WithValueType()
+    {
+        var mock = new Mock<ILogger<Benchmark>>();
+        var logger = mock.Object;
+
+        for (var i = 0; i < Count; i++)
+        {
+            logger.LogInformation("Message: {message}", 2);
+        }
+    }
+
+    [Benchmark]
+    public void ILogger_WithReferenceType()
+    {
+        var mock = new Mock<ILogger<Benchmark>>();
+        var logger = mock.Object;
+
+        for (var i = 0; i < Count; i++)
+        {
+            logger.LogInformation("Message: {message}", "reference type");
+        }
+    }
+}
---
+++ LoggerAdapter.Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using LoggerAdapter.Benchmarks;
+
+BenchmarkRunner.Run<Benchmark>();
